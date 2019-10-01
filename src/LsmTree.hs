d0389cc0b063b730205651f10c96481ad61ffd0f
miner9000 <wertufan@mail.ru>
2019-10-01 04:08:03 +0300

    [#4] Implement insert function
    Resolves #4

--- src/LsmTree.hs
+++ src/LsmTree.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DatatypeContexts #-}
 
 module LsmTree
        ( -- * Data type
@@ -10,10 +11,11 @@ module LsmTree
        , emptyTable
        , initTree
        , initTable
+       , insertTree
        ) where
 
 -- | Data type that stores values while program is working.
-data Tree a
+data Ord a => Tree a
     = TreeNode (Tree a) a (Tree a)
     | Leaf
     deriving stock (Eq, Show)
@@ -21,15 +23,24 @@ data Tree a
 emptyTree :: Tree a
 emptyTree = Leaf
 
-initTree :: a -> Tree a
+initTree :: Ord a => a -> Tree a
 initTree x = TreeNode Leaf x Leaf
 
+insertTree :: Ord a => Tree a -> a -> Tree a
+insertTree Leaf x = initTree x
+insertTree (TreeNode l val r) x
+   | x < val   = TreeNode (insertTree l x) val r
+   | otherwise = TreeNode l val (insertTree r x)
+
 -- | Data type that stores values in memory.
-data Table a = Table [a]
+data Ord a => Table a = Table [a]
     deriving stock (Eq, Show)
 
-emptyTable :: Table a
+emptyTable :: Ord a => Table a
 emptyTable = Table []
 
-initTable :: a -> Table a
+initTable :: Ord a => a -> Table a
 initTable x = Table [x]
+
+insertTable :: Ord a => Table a -> a -> Table a
+insertTable = undefined
