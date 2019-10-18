8ca17c6c9359c539d3242e30b234cc6be8703b38
miner9000 <wertufan@mail.ru>
2019-10-18 15:17:43 +0300

    [#3] Implement lookup function
    Resolves #3

--- src/LsmTree.hs
+++ src/LsmTree.hs
@@ -9,6 +9,7 @@ module LsmTree
          -- * Update
        , emptyTree
        , initTree
+       , lookupTree
        , initFileSystem
        , insertTree
        , insertTable
@@ -33,10 +34,17 @@ insertTree x tree = case tree of
         True  -> TreeNode (insertTree x l) val r
         False -> TreeNode l val (insertTree x r)
 
+lookupTree :: Ord a => a -> Tree a -> Bool
+lookupTree _ Leaf = False
+lookupTree x (TreeNode l y r)
+    | x > y  = lookupTree x r
+    | x < y  = lookupTree x l
+    | x == y = True
+
 -- | Data type that stores values in memory.
 data Table = Table
-    { fileName :: String
-    , size :: Int
+    { fileName :: FilePath
+    , size :: Int --size of a file
     } deriving stock (Eq, Show)
 
 type FileSystem = [Table]
@@ -44,9 +52,8 @@ type FileSystem = [Table]
 -- | Upload file's information to FileSystem
 initFileSystem :: FilePath -> IO ()
 initFileSystem fp = undefined -- ^ TODO
-    
 
-insertTable :: String -> [a] -> FileSystem -> IO FileSystem
+insertTable :: FilePath -> String -> FileSystem -> IO FileSystem
 insertTable name lst fs = do
     writeFile name lst
     return $ Table { fileName = name, size = length lst } : fs
